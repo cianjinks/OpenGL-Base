e9513ea849fe88ba00243a3a76edf756e9491008
cianjinks <cjinks99@gmail.com>
2020-07-28 13:27:12 +0100

    Removed uncecessary example files

--- Examples/RayTracedVoxels/assets/shaders/lightfrag.glsl
+++
@@ -1,8 +0,0 @@
-#version 410 core
-layout(location = 0) out vec4 color;
-
-uniform vec3 u_LightColor;
-void main()
-{
-	color = vec4(u_LightColor, 1.0);
-}
--- Examples/RayTracedVoxels/assets/shaders/lightvert.glsl
+++
@@ -1,9 +0,0 @@
-#version 410 core
-layout(location = 0) in vec3 a_Pos;
-
-uniform mat4 u_TransformMatrix;
-
-void main()
-{
-	gl_Position = u_TransformMatrix * vec4(a_Pos, 1.0f);
-}
